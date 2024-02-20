97ad8b1032894325e9577439a373eb08c3aa1a11
Callum Brezden <callumbrezden@gmail.com>
2024-02-19 17:23:02 -0800

    Added matches into main view

--- scenes/match.gd
+++ scenes/match.gd
@@ -1,5 +1,13 @@
 extends PanelContainer
-const MATCH_HOVER = preload("res://themes/match_hover.tres")
 
 func _on_mouse_entered():
-	print("sdfsdf")
+	var hover_highlight = Color("#92ed8e")
+	set_modulate(hover_highlight)
+
+func _on_mouse_exited():
+	var regular = Color(1,1,1,1)
+	set_modulate(regular)
+
+func _on_gui_input(event):
+	if event.is_pressed():
+		self.visible = false
