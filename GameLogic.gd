525a3413b6cf360b3368139450b35694f48c37e8
Callum Brezden <callumbrezden@gmail.com>
2024-02-23 19:53:32 -0800

    Progress on integration of match_controller into game logic

--- GameLogic.gd
+++ GameLogic.gd
@@ -1,12 +1,21 @@
 extends Node
 
-var pileMin = 3
-var pileMax = 3 # Declare a strict maximum
+@onready var matches = [$"VBoxContainer/Pile 1/Match_1", $"VBoxContainer/Pile 2/Match_2", $"VBoxContainer/Pile 2/Match_3", $"VBoxContainer/Pile 3/Match_4", $"VBoxContainer/Pile 3/Match_5", $"VBoxContainer/Pile 3/Match_6", $"VBoxContainer/Pile 4/Match_7", $"VBoxContainer/Pile 4/Match_8", $"VBoxContainer/Pile 4/Match_9", $"VBoxContainer/Pile 4/Match_10", $"VBoxContainer/Pile 4/Match_11"]
+@onready var turn_label = $turn_label
+
+var pileMin = 4
+var pileMax = 4 # Declare a strict maximum
 var matchMin = 1
-var matchMax = 8
+var matchMax = 5
 var gameArray # Keeps track of the current number of piles and matches per pile, stored in an array
-var playerTurn # Keeps track of whose turn it is, player's true if true, opponent's turn otherwise
+var player_turn # Keeps track of whose turn it is, player's true if true, opponent's turn otherwise
 var ongoingGame = false
+var player_active = false
+var match_number:int = 3
+var match_index_max = [] # Keeps track of the maximum index of a match in each pile
+var match_full_count # Keeps track of the total number of matches in the game space
+var pile_counters = {"pile_1": 0, "pile_2": 0, "pile_3": 0, "pile_4": 0}
+var valid_match_array = [] # Keeps track of the valid range of matches to remove from a pile
 
 # Create a game with a set number of piles and number of matches in each pile
 # The maximum values for piles and matches are declared at the top of this file
@@ -14,10 +23,13 @@ var ongoingGame = false
 func _create_game():
 	var piles = []
 	var numberOfPiles = randi_range(pileMin, pileMax)
-	#print("Number of piles: " + numberOfPiles)
-	#piles = [numberOfPiles]
-	for pile in numberOfPiles:
-		piles.append(randi_range(matchMin, matchMax))
+	var match_full_count = 0
+	for i in range(numberOfPiles):
+		var pile = randi_range(matchMin, matchMax)
+		match_full_count += pile
+		match_index_max.append(match_full_count)
+		piles.append(pile)
+		pile_counters["pile_" + str(i + 1)] = pile
 	return piles
 
 func _start_round():
@@ -25,17 +37,18 @@ func _start_round():
 	gameArray = _create_game()
 	# Randomly choose who goes first
 	if randi() % 2:
-		playerTurn = true
+		player_turn = true
 	else:
-		playerTurn = false
+		player_turn = false
 	ongoingGame = true
 
 func _ready():
 	_start_round()
 
 func _process(_delta):
-	if ongoingGame:
-		_game_logic(gameArray)
+	pass
+	# if ongoingGame:
+	# 	_game_logic(gameArray)
 
 func _game_logic(array):
 	# Clean up array every time a pile is emptied, remove its index from the array
@@ -44,27 +57,31 @@ func _game_logic(array):
 	# Whoever makes the last move is the winner
 	if array.is_empty():
 		ongoingGame = false
-		if playerTurn:
+		if player_turn:
 			print("You lose!")
 		else:
 			print("You win!")
 		return
 	print("Current game space is " + str(array))
-	if playerTurn:
+	if player_turn:
 		# the player's turn to take matches))
 		print("Player's turn")
+
+		valid_match_array.clear()
+		for i in match_full_count:
+			valid_match_array.append(i + 1)
 		# Insert player control here and remove _random_move()
 		_player_make_choice(array)
 		
 		# Remove this function once player choice is implemented
 		_random_move(array)
 		
-		playerTurn = false
+		player_turn = false
 	else:
 		# opponent's turn to take matches
 		print("Opponent's turn")
 		_ai_make_choice(array)
-		playerTurn = true
+		player_turn = true
 
 # Function to remove indices which contain value 0
 func _shrink_array(array):
@@ -118,3 +135,10 @@ func _random_move(array):
 	var remove = randi_range(1, array[index]) # Calculate a random number of matches to remove, minimum number allowable to remove is 1
 	array[index] -= remove
 	print(str(remove) + " matches removed from pile " + str(index + 1))
+
+func make_range():
+	valid_match_array.clear()
+	for i in range(len(match_index_max) - 1):
+		if match_number > match_index_max[i] and match_number <= match_index_max[i + 1]:
+			valid_match_array = range(match_index_max[i] + 1, match_index_max[i + 1] + 1)
+			break
